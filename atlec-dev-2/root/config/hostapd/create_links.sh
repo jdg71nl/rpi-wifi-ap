cadbae85b232909e661b453374b74ade23e597e7
John de Graaff <john@de-graaff.net>
2022-04-16 21:05:10 +0200

    some

--- atlec-dev-2/root/config/hostapd/create_links.sh
+++ atlec-dev-2/root/config/hostapd/create_links.sh
@@ -5,8 +5,10 @@ mkdir -pv config/hostapd/
 cd config/hostapd/
 
 for file in \
-  /etc/default/isc-dhcp-server /etc/network/interfaces /etc/iptables.conf /etc/default/hostapd /etc/hostapd/hostapd.conf /etc/hostapd/wlan0.conf /etc/hostapd/wlan1.conf /etc/dhcp/dhcpd.conf /etc/sysctl.conf /etc/default/dnsproxy /etc/dnsproxy.conf \ 
-  ; do echo "# file: $file ..." ; ln -sf $file . ; done
+  /etc/default/isc-dhcp-server /etc/network/interfaces /etc/iptables.conf /etc/default/hostapd /etc/hostapd/hostapd.conf /etc/hostapd/wlan0.conf /etc/hostapd/wlan1.conf /etc/dhcp/dhcpd.conf /etc/sysctl.conf /etc/default/dnsproxy /etc/dnsproxy.conf ; 
+do 
+  echo "# file: $file ..." ; ln -sf $file . ; 
+done
 
 #
 # > apt install -y isc-dhcp-server hostapd dnsproxy
